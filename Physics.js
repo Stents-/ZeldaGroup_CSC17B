98f3a37724fc7a027ead8eb5a17b9f628d668116
HarleyV <harley.vasquez@hotmail.com>
2017-10-18 17:58:51 -0700

    Update Physics.js
    completed phys function

--- Physics.js
+++ Physics.js
@@ -33,10 +33,10 @@ function phys(obj1, obj2, deltaTime) {
             var dir = obj1.velocity.normalize(); // Direction of the movement
 
             // The distance from oldPos to the collision point
-            var t = (bottom - oldPos.y) / dir.y;
+            var t1 = (bottom - oldPos.y) / dir.y;
 
             // Collision point
-            var hit = oldPos.add(dir.mul(t));
+            var hit = oldPos.add(dir.mul(t1));
 
             // Make sure the collision happens on the x axis
             if (hit.x > obj2.position.x - obj1.size.x &&
@@ -49,18 +49,78 @@ function phys(obj1, obj2, deltaTime) {
             }
 
 
-		} else if (oldPos.y - obj1.size.y > obj2.position.y) {
+		} else if (oldPos.y + obj1.size.y > obj2.position.y) {
 			// Check for intersection on upper horiz line
+          var top = obj2.position.y; // top of obstacle
+          var dir = obj1.velocity.normalize(); // Direction of the movement
+
+          // The distance from oldPos to the collision point
+          var t1 = ((oldPos.y + obj1.size.y) - top) / dir.y;
+
+          // Collision point
+          var hit = oldPos.add(dir.mul(t1));
+
+          // Make sure the collision happens on the x axis
+          if (hit.x > obj2.position.x - obj1.size.x &&
+              hit.x < obj2.position.x + obj2.size.x) {
+              console.log(hit);
+
+              // Set the position to hit and remove all velocity
+              obj1.position = hit;
+              obj1.velocity = new Vector2(0, 0);
+      }
 		}
 
 
 		if (oldPos.x > obj2.position.x + obj2.size.x) {
 			// Check for intersection on right vert line
+          var right = obj2.position.x + obj2.size.x; // Right side of obstacle
+          var dir = obj1.velocity.normalize(); // Direction of the movement
+
+          // The distance from oldPos to the collision point
+          var t2 = (oldPos.x - right) / dir.x;
+
+          // Collision point
+          var hit = oldPos.add(dir.mul(t2));
+
+          // Make sure the collision happens on the y axis
+          if (hit.y > obj2.position.y - obj1.size.y &&
+              hit.y < obj2.position.y + obj2.size.y) {
+              console.log(hit);
+
+              // Set the position to hit and remove all velocity
+              obj1.position = hit;
+              obj1.velocity = new Vector2(0, 0);
+          }
+
 		} else if (oldPos.x + obj1.size.x < obj2.position.x) {
 			// Check for intersection on left vert line
+          var left = obj2.position.x; // left side of obstacle
+          var dir = obj1.velocity.normalize(); // Direction of the movement
+
+          // The distance from oldPos to the collision point
+          var t2 = (Left - (oldPos.x + obj1.size.x)) / dir.x;
+
+          // Collision point
+          var hit = oldPos.add(dir.mul(t2));
+
+          // Make sure the collision happens on the y axis
+          if (hit.y > obj2.position.y - obj1.size.y &&
+              hit.y < obj2.position.y + obj2.size.y) {
+              console.log(hit);
+
+              // Set the position to hit and remove all velocity
+              obj1.position = hit;
+              obj1.velocity = new Vector2(0, 0);
+          }
 		}
 
 
 	}
 
+	if(t1 < t2){
+		return t1;
+	} else {
+		return t2;
+	}
 }
